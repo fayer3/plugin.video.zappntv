43ac1e2166539eff53b77c00f5189df38eec6165
fayer3 <fb4472@aon.at>
2019-07-04 18:44:59 +0200

    add copyright notice for xxtea.py

--- resources/lib/xxtea.py
+++ resources/lib/xxtea.py
@@ -12,6 +12,13 @@
 #| LastModified: Mar 10, 2015                               |
 #|                                                          |
 #\**********************************************************/
+# The MIT License (MIT)
+#
+# Copyright (c) 2008-2016 Ma Bingyao mabingyao@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 import base64
 import sys
